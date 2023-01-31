7afd35ae508ba07e570328760916d1fa174fdb06
tayumaru <44870857+tayumaru@users.noreply.github.com>
2023-01-31 11:57:40 +0100

    Resource Bar and UI
    Implemented Resource Bar
    Basic UI
    
    To test in your scene just grab the SETUP prefab, put it in a completely EMPTY scene, then unpack prefab completely, extract the various children from "SETUP" , then delete setup.

---
+++ ProjectRoots/Assets/Scripts/ResourceBar.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ResourceBar : MonoBehaviour
+{
+    [SerializeField] Slider slider;  // slider.value holds the amount of resource currently collected and owned by the player.
+
+    [SerializeField] float resourceInterval = 1f; // how much time passes between generating new resources.
+    [SerializeField] int resourceIncrement = 1; // amount of resources generated every time the timer reaches 0.
+    [SerializeField] TextMeshProUGUI resourceText;
+    [SerializeField] ParticleSystem fireworks;
+
+    int maximumResourceAllowed;
+
+    float resourceTimer;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.value = 0;
+        resourceTimer = resourceInterval;
+        maximumResourceAllowed = (int) slider.maxValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(resourceTimer <= 0)
+        {
+            IncreaseResource(resourceIncrement);
+            resourceTimer = resourceInterval;
+        }
+        else
+        {
+            resourceTimer -= Time.deltaTime; 
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            IncreaseResource(10);
+            fireworks.Play();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Backspace))
+        {
+            DecreaseResource(10);
+        }
+    }
+
+
+    void IncreaseResource(int amount)
+    {
+        if(amount > 0)
+        {
+            slider.value += amount;
+
+            if(slider.value > maximumResourceAllowed)
+            {
+                slider.value = maximumResourceAllowed;
+            }
+            resourceText.text = slider.value.ToString();
+
+        }
+    }
+
+    void DecreaseResource(int amount)
+    {
+        if(amount > 0)
+        {
+            slider.value -= amount;
+            
+            if(slider.value < 0)
+            {
+                slider.value = 0;
+            }
+
+            resourceText.text = slider.value.ToString();
+        }
+    }
+}
