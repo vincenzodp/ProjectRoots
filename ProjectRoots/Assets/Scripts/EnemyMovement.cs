e0b3add0c4c4dbec1acda4f12e7dbcbaf556f29a
Bruka93 <118062601+Bruka93@users.noreply.github.com>
2023-02-01 00:47:53 +0100

    Enemy_Movement_Alpha

--- ProjectRoots/Assets/Scripts/EnemyMovement.cs
+++ ProjectRoots/Assets/Scripts/EnemyMovement.cs
@@ -2,26 +2,26 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    //private Animation anim;
     public Transform Spawn;
+    //public Transform Spawn1;
     private Transform target;
     private Enemy enemy;
     private float startTime;
 
     void Start()
     {
-        //anim = gameObject.GetComponent<Animation>();
+
         target = GameObject.FindGameObjectWithTag("Tree").transform;
         Spawn = GameObject.Find("SpawnPoint").transform;
+        //Spawn = GameObject.Find("SpawnPoint(1)").transform;
         enemy = GetComponent<Enemy>();
         startTime = Time.time;
-        //anim.Play();
     }
 
     void Update()
     {
-        Vector3 center = (Spawn.position + target.position) * 0.8F;
-        center -= new Vector3(0, 1, 0);
+        Vector3 center = (Spawn.position + target.position) * 0.5F;
+        center -= new Vector3(0, 9, 0);
         Vector3 riseRelCenter = Spawn.position - center;
         Vector3 setRelCenter = target.position - center;
 
@@ -32,15 +32,15 @@ public class EnemyMovement : MonoBehaviour
 
         transform.position = Vector3.Slerp(riseRelCenter, setRelCenter, fracComplete);
         transform.position += center;
-        
+
         //Vector3 dir = target.position - transform.position;
         //transform.Translate(dir.normalized * enemy.speed * Time.deltaTime, Space.World);
 
-        //if (Vector3.Distance(transform.position, target.position) <= 0.4f)
-        //{
-        //    Debug.Log("Tree reached");
-        //    Destroy(enemy);
-        //}
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            Debug.Log("Tree reached");
+            Destroy(enemy);
+        }
         //enemy.speed = enemy.startSpeed;
     }
 }
